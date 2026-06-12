242b260b84cbd9350b3cd40a505ad283e109a3f2
rimahatib51-star <rimahatib51@gmail.com>
2026-06-12 20:58:30 +0300

    Add showMessage function to display project info

---
+++ script.js
@@ -0,0 +1,6 @@
+function showMessage(){
+
+    document.getElementById("message").innerHTML =
+    "Bu proje Tkinter grafik arayüzü ile çalışan Python masaüstü uygulamasıdır. Çalıştırmak için netflix_app.py dosyasını indirip Python ile çalıştırmalısınız.";
+
+}
